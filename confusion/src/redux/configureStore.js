360350848774f368b49bd86af96404b1d58f7e4f
Mauricio Hess <mauricio.leao.hess@gmail.com>
2021-05-08 09:34:24 +0200

    Redux Combining

---
+++ confusion/src/redux/configureStore.js
@@ -0,0 +1,38 @@
+/**     REDUX
+ *
+ *  # Holds the current state
+ *
+ *  # Created using createStore()
+ *
+ *  # Supplies three methods:
+ *      - dispatch(): states state update with the provided action object
+ *                      
+ *                      Dispatch method is where you will supply the 
+ *                      action object and then specify what 
+ *                      changes you want to make to the state.
+ *      
+ *      - getState(): returns the current stored state value
+ *      - subscribe(): accept a callback function that will be run every time
+ *                     an action is dispatched
+ *
+ */
+
+
+ import {createStore, combineReducers} from 'redux';
+ import { Dishes } from './dishes';
+ import { Comments } from './comments';
+ import { Promotions } from './promotions';
+ import { Leaders } from './leaders';
+ 
+ export const ConfigureStore = () => {
+     const store = createStore(
+         combineReducers({
+             dishes: Dishes,
+             comments: Comments,
+             promotions: Promotions,
+             leaders: Leaders
+         })
+     );
+ 
+     return store;
+ }
